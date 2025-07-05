66af146c1bc1186a6efbc6dbd4da57176b3b66fa
NataliaKmsk <nataliak289@gmail.com>
2025-07-06 01:24:46 +0200

    Update bruteforce.c
    dodanie sprawdzania kolmn i wierszy

--- bruteforce.c
+++ bruteforce.c
@@ -31,16 +31,13 @@ int	is_column_unique(char grid[4][4])
 	}
 	return (1);
 }
-void	solve(char *clues)
+
+//Sprawdzanie kombinacji
+int try_combinations(char **perms, char *clues)
 {
-	char *perms[24] = {
-		"1234", "1243", "1324", "1342", "1423", "1432",
-		"2134", "2143", "2314", "2341", "2413", "2431",
-		"3124", "3142", "3214", "3241", "3412", "3421",
-		"4123", "4132", "4213", "4231", "4312", "4321"
-	};
 	char grid[4][4];
 	int i = 0;
+
 	while (i < 24)
 	{
 		int j = 0;
@@ -56,11 +53,10 @@ void	solve(char *clues)
 					copy_row(grid[1], perms[j]);
 					copy_row(grid[2], perms[k]);
 					copy_row(grid[3], perms[l]);
-
 					if (is_column_unique(grid) && check_clues(grid, clues))
 					{
 						print_grid(grid);
-						return ;
+						return (1);  // znaleziono rozwiązanie
 					}
 					l++;
 				}
@@ -70,5 +66,89 @@ void	solve(char *clues)
 		}
 		i++;
 	}
-	write(1, "Error", 6);
+	return (0); // Brak rozwiazania
+}
+// Możliwości
+void solve(char *clues)
+{
+	char *perms[24] = {
+		"1234", "1243", "1324", "1342", "1423", "1432",
+		"2134", "2143", "2314", "2341", "2413", "2431",
+		"3124", "3142", "3214", "3241", "3412", "3421",
+		"4123", "4132", "4213", "4231", "4312", "4321"
+	};
+
+	if (!try_combinations(perms, clues))
+		write(1, "Error", 5);
+}
+//Funkcja pomocnicza
+int count_visible(char *line)
+{
+	int max_height = 0;
+	int visible = 0;
+	int i = 0;
+
+	while (i < 4)
+	{
+		if (line[i] > max_height)
+		{
+			max_height = line[i];
+			visible++;
+		}
+		i++;
+	}
+	return visible;
+}
+// Sprawdzanie widocznosci
+int check_clues(char grid[4][4], char *clues)
+{
+	int i;
+	char line[4];
+
+	// Sprawdzamy wskazówki z góry i dołu (kolumny)
+	i = 0;
+	while (i < 4)
+	{
+		// Z góry
+		line[0] = grid[0][i];
+		line[1] = grid[1][i];
+		line[2] = grid[2][i];
+		line[3] = grid[3][i];
+		if (clues[i] != '0' && count_visible(line) != (clues[i] - '0'))
+			return 0;
+
+		// Z dołu (odwrócone)
+		line[0] = grid[3][i];
+		line[1] = grid[2][i];
+		line[2] = grid[1][i];
+		line[3] = grid[0][i];
+		if (clues[i + 4] != '0' && count_visible(line) != (clues[i + 4] - '0'))
+			return 0;
+
+		i++;
+	}
+
+	// Sprawdzamy wskazówki z lewej i prawej (wiersze)
+	i = 0;
+	while (i < 4)
+	{
+		// Z lewej
+		line[0] = grid[i][0];
+		line[1] = grid[i][1];
+		line[2] = grid[i][2];
+		line[3] = grid[i][3];
+		if (clues[i + 8] != '0' && count_visible(line) != (clues[i + 8] - '0'))
+			return 0;
+
+		// Z prawej (odwrócone)
+		line[0] = grid[i][3];
+		line[1] = grid[i][2];
+		line[2] = grid[i][1];
+		line[3] = grid[i][0];
+		if (clues[i + 12] != '0' && count_visible(line) != (clues[i + 12] - '0'))
+			return 0;
+
+		i++;
+	}
+	return 1;
 }
